cab23e0f6f04f5abeeee56eb183cae7de49c4a6f
Kirti Shinde <kirti182001@gmail.com>
2023-05-04 19:02:56 +0530

    booking functionality given

--- backend/routes/photographer.route.js
+++ backend/routes/photographer.route.js
@@ -36,25 +36,46 @@ photographyRouter.patch("/update/:id", async (req, res) => {
 });
 
 //assending sort low to high
-photographyRouter.get('/low', async (req, res) => {
-    const photo = await photoModel.find().sort({ "charges": 1 })
-    res.send({ msg: 'photo', data: photo })
-})
+photographyRouter.get("/low", async (req, res) => {
+  const photo = await photoModel.find().sort({ charges: 1 });
+  res.send({ msg: "photo", data: photo });
+});
 
 //desending sort high to low
-photographyRouter.get('/high', async (req, res) => {
-    const photo = await photoModel.find().sort({ "charges": -1 })
-    res.send({ msg: 'photo', data: photo })
-})
+photographyRouter.get("/high", async (req, res) => {
+  const photo = await photoModel.find().sort({ charges: -1 });
+  res.send({ msg: "photo", data: photo });
+});
 //search by hotel
 photographyRouter.get("/search", async (req, res) => {
   const { location } = req.query;
   try {
-      const photos = await photoModel.find({ location: { $regex: `${location}`, $options: "i" } });
-      res.send({ msg: "photographer", data: photos });
+    const photos = await photoModel.find({
+      location: { $regex: `${location}`, $options: "i" },
+    });
+    res.send({ msg: "photographer", data: photos });
   } catch (err) {
-      console.log(err);
-      res.status(500).send({ error: "Internal Server Error" });
+    console.log(err);
+    res.status(500).send({ error: "Internal Server Error" });
   }
 });
+photographyRouter.patch("/update/:id/occasions/:occasionName/slots/:slotTime", async (req, res) => {
+  const photoId = req.params.id;
+  const occasionName = req.params.occasionName;
+  const slotTime = req.params.slotTime;
+  const update = { $set: { [`occasion.$[occasion].slots.$[slot].booked`]: true } };
+  const options = { arrayFilters: [{ "occasion.name": occasionName }, { "slot.time": slotTime }] };
+  
+  try {
+    const photo = await photoModel.findByIdAndUpdate(photoId, update, options);
+    console.log(photo);
+    res.send({ msg: "Slot booked successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: "Something went wrong" });
+  }
+});
+
+
+
 module.exports = { photographyRouter };
