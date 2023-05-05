b3aaed419b3eadd578a796fa2d7bb6ddb2c6fcad
Kirti Shinde <kirti182001@gmail.com>
2023-05-05 19:06:28 +0530

    navbar and booking form completed

--- frontend/scripts/showphotographer.js
+++ frontend/scripts/showphotographer.js
@@ -104,3 +104,23 @@ function display(data) {
 search.addEventListener("change", searchPhotos);
 select1.addEventListener("change", select);
 window.addEventListener("load", getdata);
+// navbar2
+let ham = document.getElementById("hamburger");
+ham.addEventListener("click", function () {
+    this.classList.toggle("is-active");
+    if (this.classList.contains("is-active")) {
+      openNav();
+    } else {
+      closeNav();
+    }
+  });
+  function openNav() {
+    console.log("open");
+    document.getElementById("burger").style.display = "initial";
+    document.getElementById("burger").style.transition =
+      "transform 250ms ease-in-out";
+  }
+  function closeNav() {
+    console.log("close nav");
+    document.getElementById("burger").style.display = "none";
+  }
