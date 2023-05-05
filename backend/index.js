8bfb333a3804395bfdb5508c578a8a8c7f24bc7a
unknown <Sutarimran47@gmail.com>
2023-05-05 23:45:01 +0530

    Oauth

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,82 @@
 const express = require("express");
+const jwt = require("jsonwebtoken")
 const app = express();
-const cors = require("cors");
 const { connection } = require("./db");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt")
-const {authentication} = require("./middleware/Authentication")
-const {authorise} = require("./middleware/Authorization")
-const cookieparser = require("cookie-parser")
-const {UserModel} = require("./models/user.model")
+const passport = require("passport")
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const session = require('express-session');
+const { authentication } = require("./middleware/Authentication")
+const { createClient } = require("redis");
+const client = createClient();
+client.on('error', err => console.log('Redis Client Error', err));
+const { authorise } = require("./middleware/Authorization")
+const { UserModel } = require("./models/user.model")
 const { photographyRouter } = require("./routes/photographer.route");
 const { UserRoute } = require("./routes/user.route");
+const { BlacklistModel } = require("./models/blacklist.model")
 require("dotenv").config();
-app.use(express.json());
+client.connect();
+const cors = require("cors");
 app.use(cors());
-app.use(cookieparser())
 
+app.use(session({
+  secret: "my-secret-key",
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+
+app.use(express.json());
 app.use("/photographer", photographyRouter);
 
-app.get("/",(req,res)=>{
-  res.send("Welcome")
-} )
+app.use("/User", UserRoute)
 
-// app.use("/User", UserRoute)
+app.use(passport.initialize())
+app.use(passport.session());
+require("./OAuth")
 
-// app.use(authentication)
 
-app.get("/user", async(req,res)=>{
-try {
-  const data= await UserModel.find()
-  // console.log(req.cookies)
-  res.send(data)
-} catch (error) {    
-  res.send(error.message)
-}
+app.get("/google", passport.authenticate("google", { scope: ["profile","email"] }))
+
+app.get("/google/callback", passport.authenticate("google", {  failureRedirect: "/login" }), async (req, res) => {
+    const { id, displayName, emails } = req.user;
+    let usr= await UserModel.find({ "email":req.user.emails[0].value })
+    const token = jwt.sign({ userId: usr[0]._id }, 'imran', { expiresIn: '1h' });
+    client.set("token",token)
+    console.log({"token":token})
+    console.log({"mail":req.user.emails[0].value})
+    res.redirect('http://127.0.0.1:5500/frontend/index.html');
+    res.end({"token":token})
+
 })
+
+
+app.use(authentication)
+
+
+app.get("/user", async (req, res) => {
+  try {
+    const data = await UserModel.find()
+    res.send(data)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+
+app.post("/block", async (req, res) => {
+  const token = await req.cookies.Token
+  try {
+    let data = new BlacklistModel({ token })
+    await data.save()
+    res.send({ "msg": "Blacklist added", token })
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+
 
 app.listen(process.env.port, async () => {
   try {
--- backend/middleware/Authentication.js
+++ backend/middleware/Authentication.js
@@ -1,27 +1,41 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
-const {UserModel} = require("../models/user.model")
+const { UserModel } = require("../models/user.model")
+const { BlacklistModel } = require("../models/blacklist.model")
+const { createClient } = require("redis");
+// const cors = require("cors");
+// app.use(cors());
+const client = createClient();
+client.on('error', err => console.log('Redis Client Error', err));
+client.connect();
 
-const authentication= async (req,res,next)=>{
-    const token= await req.cookies.Token
-    console.log(token)
-    if(token){
-       jwt.verify(token, 'imran', async(err, decoded)=> {
-            if(decoded){
-                req.body.user=decoded.userId
-                req.user= await UserModel.find({_id:decoded.userId})
+const authentication = async (req, res, next) => {
+    
+    const token = await client.get('token')
+    console.log({"msg":"middleware" , "data":token})
+
+    if (token) {
+        const blocked = await BlacklistModel.find({ token })
+        if (blocked.length > 0) {
+            res.end("Your Login expired, Please login again")
+        }
+        jwt.verify(token, 'imran', async (err, decoded) => {
+            if (decoded) {
+                req.body.user = decoded.userId
+                req.user = await UserModel.find({ _id: decoded.userId })
+                console.log("Next")
                 next()
-            }   
-            else{
+            }
+            else {
                 res.end("Please login first")
             }
-          });
+        });
     }
-    else{
+    else {
         res.send("Login first")
     }
 }
 
-module.exports={
+module.exports = {
     authentication
 }
