59f530534845e4721ed3f2fc967ea82e566eb645
unknown <Sutarimran47@gmail.com>
2023-05-06 14:50:04 +0530

    day4

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const { UserModel } = require("./models/user.model")
 const { photographyRouter } = require("./routes/photographer.route");
 const { UserRoute } = require("./routes/user.route");
 const { BlacklistModel } = require("./models/blacklist.model")
-const {appointmentRouter} = require("./routes/appointment.route")
+const { appointmentRouter } = require("./routes/appointment.route")
 require("dotenv").config();
 client.connect();
 const cors = require("cors");
@@ -30,7 +30,7 @@ app.use(session({
 
 app.use(express.json());
 app.use("/photographer", photographyRouter);
-app.use("/appointment",appointmentRouter)
+app.use("/appointment", appointmentRouter)
 
 
 app.use("/User", UserRoute)
@@ -40,18 +40,24 @@ app.use(passport.session());
 require("./OAuth")
 
 
-app.get("/google", passport.authenticate("google", { scope: ["profile","email"] }))
+app.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }))
 app.use("/User", UserRoute)
 
-app.get("/google/callback", passport.authenticate("google", {  failureRedirect: "/login" }), async (req, res) => {
-    const { id, displayName, emails } = req.user;
-    let usr= await UserModel.find({ "email":req.user.emails[0].value })
-    const token = jwt.sign({ userId: usr[0]._id }, 'imran', { expiresIn: '1h' });
-    client.set("token",token)
-    console.log({"token":token})
-    console.log({"mail":req.user.emails[0].value})
-    res.redirect('http://127.0.0.1:5500/frontend/index.html');
-    res.end({"token":token})
+app.get("/google/callback", passport.authenticate("google", { failureRedirect: "/login" }), async (req, res) => {
+  const { id, displayName, emails } = req.user;
+  // let usr= await UserModel.find({ "email": req.user.emails[0].value })
+  const users = new UserModel({ firstName:req.user._json.given_name , lastName:req.user._json.family_name, mobileNo:00000 , email:req.user._json.email , password:"SignUp with Google OAuth" , role:"User" })
+  await users.save()
+
+  let usr = await UserModel.find({ "email": req.user._json.email })
+  console.log(req.user._json.email)
+  console.log(usr)
+  const token = jwt.sign({ userId: usr[0]._id }, 'imran', { expiresIn: '1h' });
+  client.set("token", token)
+  console.log({ "token": token })
+  console.log({ "mail": req.user.emails[0].value })
+  res.redirect('http://127.0.0.1:5500/frontend/index.html');
+  res.end({ "token": token })
 
 })
 
@@ -68,7 +74,6 @@ app.get("/user", async (req, res) => {
   }
 })
 
-
 app.post("/block", async (req, res) => {
   const token = await req.cookies.Token
   try {
@@ -79,8 +84,6 @@ app.post("/block", async (req, res) => {
     res.send(error)
   }
 })
-
-
 
 app.listen(process.env.port, async () => {
   try {
